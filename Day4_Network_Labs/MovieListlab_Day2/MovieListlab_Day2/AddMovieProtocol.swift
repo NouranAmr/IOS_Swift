3e35dc0f18ffec3fe8417a6c31b5fd4917cfd31c
NouranAmr <nouranamr206@gmail.com>
2019-05-06 15:54:24 +0200

    Day4_lab

---
+++ Day4_Network_Labs/MovieListlab_Day2/MovieListlab_Day2/AddMovieProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  AddMovieProtocol.swift
+//  MovieListlab_Day2
+//
+//  Created by Esraa Hassan on 5/6/19.
+//  Copyright © 2019 ITI. All rights reserved.
+//
+
+import Foundation
+protocol AddMovieProtocol {
+    func addNewMovie(newMovie : Movie)
+}
